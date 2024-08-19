6e891f7a5e07c0316c9faa0bc162190208433167
Moath <mkaawaja5000@gmail.com>
2024-08-19 16:46:14 +0300

    test 4

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,57 +1,60 @@
 // authService.ts
 
-import UserService from './user.service';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { User } from '../models';
-import { injectable, inject } from "tsyringe";
-import { addToBlacklist, isTokenBlacklisted } from '../helpers/tokenBlacklist';
+import UserService from './user.service'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { User } from '../models'
+import { injectable, inject } from 'tsyringe'
+import { addToBlacklist, isTokenBlacklisted } from '../helpers/tokenBlacklist'
 
 @injectable()
 export default class AuthService {
-    constructor(@inject(UserService) private userService: UserService) {}
+  constructor(@inject(UserService) private userService: UserService) {}
 
-    public async login(email: string, password: string): Promise<string> {
-        const user = await this.userService.getUserByEmail(email);
+  public async login(email: string, password: string): Promise<string> {
+    const user = await this.userService.getUserByEmail(email)
 
-        if (!user) {
-            throw new Error('Invalid credentials');
-        }
-
-        const isPasswordValid = await bcrypt.compare(password, user.password);
-        if (!isPasswordValid) {
-            throw new Error('Invalid credentials');
-        }
-
-        const token = jwt.sign(
-            { id: user.id, role: user.role },
-            process.env.JWT_SECRET || "jwt_secret",
-            { expiresIn: '7d' }
-        );
-
-        return token;
+    if (!user) {
+      throw new Error('Invalid credentials')
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+    if (!isPasswordValid) {
+      throw new Error('Invalid credentials')
     }
 
-    public async register(
-        name: string,
-        email: string,
-        address: string,
-        password: string,
-        // role: string
-    ): Promise<User> {
-        const existingUser = await this.userService.getUserByEmail(email);
-        if (existingUser) {
-            throw new Error('User already exists');
-        }
+    const token = jwt.sign(
+      { id: user.id, role: user.role },
+      process.env.JWT_SECRET || 'jwt_secret',
+      { expiresIn: '7d' }
+    )
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = { name, email, password: hashedPassword, address, role: 'user' };
-        return await this.userService.createUser(newUser);
+    return token
+  }
+
+  public async register(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<User> {
+    const existingUser = await this.userService.getUserByEmail(email)
+    if (existingUser) {
+      throw new Error('User already exists')
     }
 
-    public async logout(token: string): Promise<void> {
-        if (!isTokenBlacklisted(token)) {
-            addToBlacklist(token);
-        }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const newUser = {
+      name,
+      email,
+      address: 'put address',
+      password: hashedPassword,
+      role: 'user',
     }
+    return await this.userService.createUser(newUser)
+  }
+
+  public async logout(token: string): Promise<void> {
+    if (!isTokenBlacklisted(token)) {
+      addToBlacklist(token)
+    }
+  }
 }
