6558e35cea99aa56041850f34132bda0984b5a4b
ibrahim hamshari <lomeseth1@gmail.com>
2024-08-27 20:39:12 +0300

    init

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import sequelize from './config/db'
 import swaggerUI from 'swagger-ui-express'
 import * as swaggerDocument from './swagger.json'
+import client from 'prom-client'
 import {
   cartRouter,
   categoryRouter,
@@ -20,11 +21,41 @@ import {
 import cors from 'cors'
 dotenv.config()
 
+const register = new client.Registry();
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
+collectDefaultMetrics({
+  register
+})
+const counter = new client.Counter({
+  name: 'http_request_count',
+  help: 'Count of HTTP requests',
+  labelNames: ['method', 'route', 'statusCode']
+});
+
 const app = express()
 const PORT = process.env.PORT || 3000
+
+
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+
+
+
+register.registerMetric(counter);
+
+app.use(function (req:Request, res:Response, next:NextFunction) {
+  counter.labels({
+    method: req.method,
+    route: req.originalUrl,
+    statusCode: res.statusCode
+  }).inc();
+  next();
+});
+
+
 app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
@@ -38,6 +69,11 @@ app.use('/api/discounts', discountRouter)
 app.use('/api/brands', brandRouter)
 app.use('/api/dashboard', dashboardRouter)
 app.use('/api/address', addressRouter)
+app.get("/metrics", async (req: Request, res: Response) => {
+  res.setHeader("Content-Type", client.register.contentType);
+  let metrics = await register.metrics();
+  res.send(metrics);
+});
 const startServer = async () => {
   try {
     await sequelize.authenticate()
