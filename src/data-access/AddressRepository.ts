ad7b456698d0aa21b359cbc7b47929525da3fae6
Yousef Ali <yousefkmail169@gmail.com>
2024-08-28 11:15:45 +0300

    edit:add documentation to addressrepository

--- src/data-access/AddressRepository.ts
+++ src/data-access/AddressRepository.ts
@@ -7,6 +7,14 @@ export class AddressRepository
   extends RepositoryBase<Address>
   implements IAddressRepository
 {
+  /**
+   *
+   * @param id id of the address to be retrieved
+   * @param userId id of the user owning this address.
+   * @throws Error when it fails to retrieve the data.
+   * @returns Address if any found, null otherwise,
+   *
+   */
   async getAddressByIdAndUserId(
     id: number,
     userId: number
@@ -16,10 +24,29 @@ export class AddressRepository
       where: { id, userId },
     })
   }
+
+  /**
+   *
+   * @param id id of the address to be deleted
+   * @param userId id of the user owning the address.
+   * @throws Error when it fails to delete.
+   * @returns true if the deletion were successful, otherwise false.
+   *
+   */
+
   async deleteAddress(id: number, userId: number): Promise<boolean> {
     const number = await this.model.destroy({ where: { id, userId } })
     return number > 0
   }
+
+  /**
+   *
+   * @param id id of the address to be updated
+   * @param userId id of the user owning this address.
+   * @param data new data for the address to be updated.
+   * @throws Error when it fails to update the address.
+   * @returns Address when it successfully updates a corresponding address, null otherwise
+   */
   async updateAddress(
     id: number,
     userId: number,
@@ -35,6 +62,11 @@ export class AddressRepository
     return updatedEntity
   }
 
+  /**
+   *
+   * @param userId user Id of the addresses
+   * @returns list of addresses for the user.
+   */
   async getAddressesByUserId(userId: number): Promise<Address[]> {
     return await this.model.findAll({
       attributes: { exclude: ['userId'] },
